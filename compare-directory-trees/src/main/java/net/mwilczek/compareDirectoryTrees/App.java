48175c7568978ff0d1bc3ef9b3500af7669dbd0c
mwilczek-net <mwilczek-net@users.noreply.github.com>
2020-01-09 23:04:50 +0100

    Empty app

---
+++ compare-directory-trees/src/main/java/net/mwilczek/compareDirectoryTrees/App.java
@@ -0,0 +1,13 @@
+package net.mwilczek.compareDirectoryTrees;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
